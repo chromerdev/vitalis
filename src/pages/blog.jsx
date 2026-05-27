fe76b0378893977e25f92d842a8061330aade361
Gabriel Nunes Ferreira <gabrielnunesferreira75@gmail.com>
2026-05-27 13:57:58 -0400

    Criando filtros para temas, acressentando mais posts e evento de click para redirecionar pagina.

--- src/pages/blog.jsx
+++ src/pages/blog.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import {
   Container,
   Card,
@@ -18,6 +19,8 @@ const posts = [
     data: "Atualizado em 20/05/2026",
     imagem:
       "https://images.unsplash.com/photo-1515377905703-c4788e51af15",
+      url:
+      "https://www.amesuamente.org.br/blog/cinco-mitos-sobre-terapia/"
   },
   {
     id: 2,
@@ -28,6 +31,8 @@ const posts = [
     data: "Atualizado em 19/05/2026",
     imagem:
       "https://images.unsplash.com/photo-1532187863486-abf9dbad1b69",
+      url:
+      "https://www.einstein.br/n/vida-saudavel/quais-sao-e-o-que-fazem-os-orgaos-do-sistema-nervoso"
   },
   {
     id: 3,
@@ -38,10 +43,79 @@ const posts = [
     data: "Atualizado em 18/05/2026",
     imagem:
       "https://images.unsplash.com/photo-1584308666744-24d5c474f2ae",
+      url:
+      "https://www.einstein.br/n/vida-saudavel/por-que-o-uso-do-clobutinol-e-contraindicado-entenda-os-riscos"
+  },
+  {
+    id: 4,
+    categoria: "Saúde da mulher",
+    titulo: "Quais os melhores execicios para quem estar gravido?",
+    descricao:
+      "Autoridades sanitárias suspenderam o registro do medicamento...",
+    data: "Atualizado em 26/05/2026",
+    imagem:
+      "https://imgs.search.brave.com/nKyBcg4rLSlQHXuFvDPA0pJAjl_W0REDakZaBKSoFgc/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9ibG9n/LmF0aGxldGljLmNv/bS5ici93cC1jb250/ZW50L3VwbG9hZHMv/MjAyMy8wNi9hdGl2/aWRhZGVzLWZpc2lj/YXMtcGFyYS1tdWxo/ZXJlcy1ncmF2aWRh/cy0xLS5wbmc_eDY5/MzYz",
+      url:
+      "https://www.tuasaude.com/melhores-exercicios-para-praticar-na-gravidez/"
+  },
+  {
+    id: 5,
+    categoria: "Dr. Responde",
+    titulo: "Qual a diferença entre whey concentrado, isolado e hidrosilado?",
+    descricao:
+      "Autoridades sanitárias suspenderam o registro do medicamento...",
+    data: "Atualizado em 03/05/2026",
+    imagem:
+      "https://imgs.search.brave.com/S_mDz384eE1tDGi2AFEaIBij2_1fm-TYs3cQJylT4f8/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9ldmlk/YS5jb20uYnIvY2Ru/L3Nob3AvYXJ0aWNs/ZXMvd2hleS1jb25j/ZW50cmFkby1pc29s/YWRvLWhpZHJvbGlz/YWRvLndlYnA_dj0x/NzczNDk0NjYwJndp/ZHRoPTExMDA",
+      url:
+      "https://g1.globo.com/saude/bem-estar/noticia/2025/08/26/qual-whey-protein-e-melhor-concentrado-isolado-ou-hidrolisado-veja-diferencas-e-saiba-qual-escolher.ghtml"
+  },
+  {
+    id: 6,
+    categoria: "Condições de Saúde",
+    titulo: "Eu tenho problemas de hipertensão quais são os melhores exercícios?",
+    descricao:
+      "Autoridades sanitárias suspenderam o registro do medicamento...",
+    data: "Atualizado em 21/05/2026",
+    imagem:
+      "https://images.unsplash.com/photo-1584308666744-24d5c474f2ae",
+      url:
+      "https://imgs.search.brave.com/t2v0XHkmNCS-HvvTjmSA2gU2C7eeqPts_U9ZL9RuaVc/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9tZWRp/YS51bmltZWRjYW1w/aW5hcy5jb20uYnIv/NGMxZWQ3M2QtMTVj/Zi00Y2Q3LWJmYzUt/OGEzMDM4OTNjMWU5"
+  },
+    {
+    id: 7,
+    categoria: "Alimentação",
+    titulo: "Tenho pré-diabetes, qual alimentação eu devo seguir?",
+    descricao:
+      "Autoridades sanitárias suspenderam o registro do medicamento...",
+    data: "Atualizado em 21/05/2026",
+    imagem:
+      "https://imgs.search.brave.com/1W8Q8tmUZCd9xl1QOOXDErOTKydjL2FNL1aQTmWM9l4/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9kaWFi/ZXRlcy5vcmcuYnIv/d3AtY29udGVudC91/cGxvYWRzLzIwMjEv/MDUvZGVzY29icmkt/cXVlLXRlbmhvLWRp/YWJldGVzLWNvbW8t/ZGV2ZXJhLWZpY2Fy/LW1pbmhhLWFsaW1l/bnRhY2FvLmpwZw",
+      url:
+      "https://www.raianecrespo.com.br/alimentacao-em-diabetes-e-pre-diabetes/"
+  },
+    {
+    id: 8,
+    categoria: "Alimentação",
+    titulo: "Qual dieta meu filho deveria seguir para crecer saudavel?",
+    descricao:
+      "Autoridades sanitárias suspenderam o registro do medicamento...",
+    data: "Atualizado em 21/05/2026",
+    imagem:
+      "https://imgs.search.brave.com/klqPSx3bdJkn-U6PclHDqDhy9-lS5XNLG4M6ZreK3AY/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9vZG91/dG9yLmNvbS5ici93/cC1jb250ZW50L3Vw/bG9hZHMvMjAyMy8w/OC9vZG91dG9yLWFs/aW1lbnRhY2FvLWlu/ZmFudGlsLjItMTAy/NHg1NzYuanBn",
+      url:
+      "https://institutopensi.org.br/dez-passos-para-uma-alimentacao-saudavel-para-seu-filho"
   },
 ];
 
 function Blog() {
+
+  const [categoriaSelecionada, setCategoriaSelecionada] = useState("");
+
+  const postsFiltrados = categoriaSelecionada ? posts.filter(
+    (post) => post.categoria === categoriaSelecionada
+  ): posts;
+
   return (
     <Container maxWidth="lg" sx={{ py: 5 }}>
       {/* Cabeçalho */}
@@ -64,13 +138,14 @@ function Blog() {
           mb: 4,
         }}
       >
-        <Chip label="Saúde da mulher" color="primary" />
-        <Chip label="Condições de Saúde" color="secondary" />
-        <Chip label="Dr. Responde" color="warning" />
-        <Chip label="Gravidez e primeira infância" color="success" />
+        <Chip label="Saúde da mulher" color="primary" onClick={() => setCategoriaSelecionada("Saúde da mulher")}/>
+        <Chip label="Condições de Saúde" color="secondary" onClick={() => setCategoriaSelecionada("Condições de Saúde")}/>
+        <Chip label="Dr. Responde" color="warning" onClick={() => setCategoriaSelecionada("Dr. Responde")}/>
+        <Chip label="Alimentação" color="success" onClick={() => setCategoriaSelecionada("Alimentação")}/>
+        <Chip label="Todos" variant="outlined" color="black" clickable onClick={() => setCategoriaSelecionada("")}/>
       </Box>
 
-      {/* GRID CORRETO */}
+      {/* GRID*/}
       <Box
         sx={{
           display: "grid",
@@ -82,11 +157,11 @@ function Blog() {
           },
 
           gap: 3,
-        }}
-      >
-        {posts.map((post) => (
+        }}>
+        {postsFiltrados.map((post) => (
           <Card
             key={post.id}
+            onClick={() => window.open(post.url, "_blank")}
             sx={{
               height: 430,
               borderRadius: 4,
